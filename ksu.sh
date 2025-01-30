764bea67f62f9fc09c62b78c0699c434b1f17818
cabagekiller <cabagekiller@gmail.com>
2025-01-30 03:08:53 -0500

    Update ksu.sh

--- ksu.sh
+++ ksu.sh
@@ -1,3 +1,3 @@
 rm -rf KernelSU-Next
 curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -s next
-cp -rf "0001-Implement-SUSFS-v1.5.3-universal.patch" KernelSU-Next && cd ./KernelSU-Next && patch -p1 -f < "0001-Implement-SUSFS-v1.5.3-universal.patch" && cd ..
+cp -rf "0001-Implement-SUSFS-v1.5.3-universal.patch" KernelSU-Next && cd ./KernelSU-Next && patch -p1 -f < "0001-Implement-SUSFS-v1.5.3-universal.patch" || true && cd ..
