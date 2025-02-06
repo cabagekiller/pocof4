a677d847987405a455be94c5bde72c47792a8f2b
cabagekiller <cabagekiller@gmail.com>
2025-02-05 19:30:54 -0500

    Update sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1241,8 +1241,9 @@ static int override_release(char __user *release, size_t len)
 }
 
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-extern int susfs_spoof_uname(struct new_utsname* tmp);
+extern void susfs_spoof_uname(struct new_utsname* tmp);
 #endif
+
 
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
